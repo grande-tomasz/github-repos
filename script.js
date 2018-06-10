02ba2c20f69f98961d7d5b8ff441022015b8c4a4
Tomasz Grande <grande.tomasz@gmail.com>
2018-06-10 18:09:36 +0200

    Get dataset information from repos element

--- script.js
+++ script.js
@@ -2,6 +2,26 @@
  * Function that runs when the whole document is loaded
  */
 const load = () => {
-  console.log("load event detected!");
+  // get all elements with repos tag
+  const repos = document.querySelectorAll("repos");
+
+  // loop through each repos element
+  repos.forEach(repoElement => {
+    try {
+      if (repoElement.dataset.user) {
+        const user = repoElement.dataset.user;
+        console.log(user);
+      } else {
+        throw "No user data specified in repos element";
+      }
+    } catch (e) {
+      // log when repos element did not have user information
+      console.log(e, repoElement);
+      return;
+    }
+    // when there is no update data then it gets undefined
+    const update = repoElement.dataset.update;
+    console.log(update);
+  });
 };
 window.onload = load;
